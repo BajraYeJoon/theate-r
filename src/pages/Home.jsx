dbb9c161a313fb4c25d4137da656fea8c3d48b71
BajraYeJoon <binaya.bajrashakya@gmail.com>
2022-03-12 11:03:04 +0545

    Fetched data in input using imdb-api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-import MainLayout from '../components/MainLayout'
+import React, { useState } from "react";
+import MainLayout from "../components/MainLayout";
 export const Home = () => {
-  return (
-    <MainLayout>this is the home page</MainLayout>
-  )
-}
+  const [input, setInput] = useState("");
 
-export default Home
+  const onSearch = () => {
+    fetch(`https://imdb-api.com/en/API/SearchAll/k_7ik12w4q/${input}`)
+      .then((res) => res.json())
+      .then((result) => console.log(result));
+  };
+
+  const inputChange = (e) => {
+    console.log(e.target.value);
+    setInput(e.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.code === "Enter") {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainLayout>
+      <input
+        type="text"
+        onChange={inputChange}
+        onKeyDown={onKeyDown}
+        value={input ?? ""}
+      />
+      <button type="button" onClick={onSearch}>
+        {" "}
+        Search
+      </button>
+    </MainLayout>
+  );
+};
+
+export default Home;
