62902b15e085f0f3cf960ddc7b6abc5682c93ae8
BajraYeJoon <binaya.bajrashakya@gmail.com>
2022-03-16 09:52:40 +0545

    Data loading and error handle

--- src/pages/ShowDesc.jsx
+++ src/pages/ShowDesc.jsx
@@ -4,16 +4,45 @@ import { apiGet } from "../config/config";
 
 export const ShowDesc = () => {
   const [showDesc, setShowDesc] = useState(null);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then((res) => {
-      setShowDesc(res);
-    });
+    let isMount = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then((res) => {
+        setTimeout(() => {
+          if (isMount) {
+            setShowDesc(res);
+            setIsLoading(false);
+          }
+        }, 2000);
+      })
+      .catch((err) => {
+        setTimeout(() => {
+          if (isMount) {
+            setIsError(err.message);
+            setIsLoading(false);
+          }
+        }, 2000);
+      });
+
+    return () => {
+      isMount = false;
+    };
   }, [id]);
 
   console.log(showDesc);
 
-  return <div></div>;
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (isError) {
+    return <div> error : {isError}</div>;
+  }
+
+  return <div>this is show page</div>;
 };
