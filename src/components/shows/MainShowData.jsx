0457ca8aacc4b73c430fdd464abd8c19a124e383
BajraYeJoon <binaya.bajrashakya@gmail.com>
2022-03-21 09:37:05 +0545

    .

--- src/components/shows/MainShowData.jsx
+++ src/components/shows/MainShowData.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import NOT_FOUND from "../../images/not-found.png";
-import {Star } from '../styled'
+import { Star } from '../styled'
 
 const MainShowData = ({ name, rating, summary, tags, image }) => {
     return (
       <div>
-        <img src={image ? image.original : NOT_FOUND} alt="show-cover" />
+        <img src={image ? image.medium : NOT_FOUND} alt="show-cover" />
         <div>
           <div>
             <h1>{name}</h1>
